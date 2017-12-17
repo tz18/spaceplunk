21e459c8ad74be8d30e941056858d9e9a278fd3d
RDRKW <thezeus18@yahoo.ca>
2017-12-16 17:02:44 -0800

    first commit: 2d unity scene, gravitational attraction between bodies with attached "attractor" components as from Brackeys tutorial ( https://youtu.be/Ouu3D_VHx9o )

---
+++ Assets/Attractor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour {
+
+    public static List<Attractor> attractors;
+    const float G = 6.674f;
+    const float distancescale = 1;
+    const float massscale = 1;
+    public Rigidbody2D rb;
+
+    void FixedUpdate() {
+        foreach (Attractor attractor in attractors) {
+            if (attractor != this) {
+                Attract(attractor);
+            }
+        }
+    }
+
+    void OnEnable() {
+        if (attractors == null)
+            attractors = new List<Attractor>();
+        attractors.Add(this);
+    }
+
+    void OnDisable() {
+
+        attractors.Remove(this);
+    }
+
+    void Attract(Attractor objToAttract) {
+        Rigidbody2D rbToAttract = objToAttract.rb;
+        Vector2 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+        if (distance == 0) {
+            return;
+        }
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass / Mathf.Pow(distance,2));
+        Vector2 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+    }
+}
